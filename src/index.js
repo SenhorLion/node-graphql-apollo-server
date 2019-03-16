dbd8e700b7a2977e3dd0ec126c326ea4d58a2cbe
Lionel Adams <adamslionel@hotmail.co.uk>
2019-03-16 11:16:38 +0000

    feat: basic implementation of express, apollo server with graphql api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import 'dotenv/config';
 
-const userCredentials = { firstname: 'Lionel' };
-const userDetails = { nationality: 'Dual' };
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
+const app = express();
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Lionel Kung Fu',
+      };
+    },
+  },
 };
 
-console.log(user);
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
 
-console.log(process.env.SOME_ENV_VARIABLE);
+// Add Express as middleware, and specify path to graphql API
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
