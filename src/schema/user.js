e3e4d727e15da398ec10b88cb2126c907869a6b0
Lionel Adams <adamslionel@hotmail.co.uk>
2019-05-16 20:39:56 +0100

    wip: domain separation pt 1

---
+++ src/schema/user.js
@@ -0,0 +1,16 @@
+import { gql } from 'apollo-server-express';
+
+export default gql`
+  extend type Query {
+    me: User
+    user(id: ID!): User
+    users: [User!]
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String
+    messages: [Message!]
+  }
+`;
